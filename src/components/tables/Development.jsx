31019686f362b24aa62acef1a8442b0402bf890b
miloupark <helenapark96@naver.com>
2023-10-23 16:03:23 +0900

    development

--- src/components/tables/Development.jsx
+++ src/components/tables/Development.jsx
@@ -53,59 +53,218 @@ const data = [
     number: 25.4,
     percentage: 75.5,
   },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
+  {
+    title: 'Marketplace',
+    image: [
+      {
+        src: apple, 
+        alt:'apple',
+      },
+      {
+        src: android,
+        alt: 'android'
+      },
+      {
+        src: windows,
+        alt: 'windows'
+      },
+    ],
+    number: 25.4,
+    percentage: 75.5,
+  },
 ];
 
 const Development = () => {
   return (
     <Card>
-    <CardHeader>
-      <Heading size='md'>Client Report</Heading>
-    </CardHeader>
+      <CardHeader>
+        <Heading size='md'>Development Table</Heading>
+      </CardHeader>
   
     <CardBody py={2} px={5}>
-    <TableContainer overflowX={"hidden"} overflowY={"auto"} maxHeight={"400px"}>
-    {/* size={['sm','md','lg']} */}
-    {/* 반응형 = 모바일, 태블릿, 데스크탑 */}
-      <Table variant='simple'>
-        <TableCaption>Development Table</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Title</Th>
-            <Th>Title</Th>
-            <Th>Title</Th>
-            <Th isNumeric>Title</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {data.map((item, index) => (
-            <Tr key={index}>
-             <Td>{item.title}</Td>
-             <Td>
-               <HStack spacing={1} alignItems={'center'}>
-                {item.image.map((images, i) => (
-                  <Image key={i} src={images.src} alt={images.alt}/>
-                ))}
-               </HStack>
-             </Td>
-             <Td>
-              <HStack/>{item.number}
-             <Td>
-              <Flex>
-
-              <Box w="32px" textAlign={'right'}>
-              {item.percentage}
-              </Box>
-             <Text variant="txt104" color= "#999">
-                %
-             </Text>
-             <Progress value={80}/>
-              </Flex>
-             </Td>
-           </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+      <TableContainer overflowX={"hidden"} overflowY={"auto"} maxHeight={"296px"}>
+      {/* size={['sm','md','lg']} */}
+      {/* 반응형 = 모바일, 태블릿, 데스크탑 */}
+        <Table variant='simple'>
+          <TableCaption>Development Table</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Title</Th>
+              <Th>Title</Th>
+              <Th isNumeric>Title</Th>
+              <Th>Title</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((item, index) => (
+              <Tr key={index}>
+                <Td>{item.title}</Td>
+                <Td>
+                  <HStack spacing={1} alignItems={'center'}>
+                    {item.image.map((images, i) => (
+                      <Image key={i} src={images.src} alt={images.alt}/>
+                    ))}
+                  </HStack>
+                </Td>
+                <Td>{item.number}</Td>
+                <Td>{item.percentage}%</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>   
     </CardBody>
     </Card>
   );
